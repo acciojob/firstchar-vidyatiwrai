fe54bcbc5a4dc0dca3fcbfab6a050d91d3c6e6a4
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-05-03 13:58:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function firstChar(text) {
   // your code here
+	const str = 'abc efg';
+const str2 = str.charAt(0).toUpperCase() + str.slice(1);
+console.log(str2);
 }
 
 // Do not change the code below
